49ea834789704f4ab5eacc53678d990949c52f92
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-12-03 19:21:33 +0800

    上传背景图片

---
+++ FootprintWalker-FrontEnd/src/components/LoginFront.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  mimimi
+  </div>
+
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
