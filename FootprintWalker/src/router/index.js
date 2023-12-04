6f31afabf8493c402a176ff29c2aee61472e5c1f
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-12-05 00:09:20 +0800

    Update index.js

--- FootprintWalker/src/router/index.js
+++ FootprintWalker/src/router/index.js
@@ -24,17 +24,11 @@ const routes = [
       {
         path: '/register',
         name: 'Register',
-        // route level code-splitting
-        // this generates a separate chunk (Home-[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import('@/views/Register.vue'),
       },
       {
         path: '/set-code',
         name: 'SetCode',
-        // route level code-splitting
-        // this generates a separate chunk (Home-[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import('@/views/SetCode.vue'),
       },
     ],
