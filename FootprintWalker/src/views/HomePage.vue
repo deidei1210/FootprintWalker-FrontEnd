db2f091941a934502e599b3f94a3964895f91b7a
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-12-16 13:30:52 +0800

    至少能显示地图了

--- FootprintWalker/src/views/HomePage.vue
+++ FootprintWalker/src/views/HomePage.vue
@@ -2,7 +2,12 @@
     <!-- 顶部的导航栏 -->
     <DefaultBar />
     <v-img src="../assets/HomeTitle.png" style="position:relative;top:-60px;"></v-img>
-    <div style="position:relative;top:-245px;"><v-img src="../assets/往期回顾.png"></v-img></div>
+    <div style="position:relative;top:-245px;">
+        <v-img src="../assets/往期回顾.png">
+            <div id="allmap"></div>
+            123456
+        </v-img>
+    </div>
     <v-row style="position:relative;top:-210px;">
         <v-col style="padding:50px;">
             <v-img src="../assets/活动报名.png"></v-img>
@@ -18,6 +23,7 @@
 <script>
 import DefaultBar from '@/layouts/default/AppBar.vue'
 import Footer from '@/layouts/default/Foot.vue'
+import { BaiduMap } from 'vue-baidu-map-3x'
 // import DefaultView from '@/layouts/default/View.vue'
 export default {
     //导出组件
@@ -36,6 +42,11 @@ export default {
         scripts.forEach(script => {
             script.parentNode.removeChild(script)
         })
+        // GL版命名空间为BMapGL
+        // 按住鼠标右键，修改倾斜角和角度
+        var map = new BMapGL.Map("allmap");    // 创建Map实例
+        map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+        map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
     },
     methods: {
 
@@ -43,3 +54,11 @@ export default {
 }
 </script>
 
+<style>
+#allmap {
+    position:relative;
+    top:230px;
+    height: 530px;
+    width: 50%;
+}
+</style>
