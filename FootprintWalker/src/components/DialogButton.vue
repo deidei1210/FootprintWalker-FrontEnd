c4bd2029e1396907a2b67aaa0f27fcef75307b32
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2024-01-12 02:53:21 +0800

    可以成功查看反馈

--- FootprintWalker/src/components/DialogButton.vue
+++ FootprintWalker/src/components/DialogButton.vue
@@ -7,24 +7,25 @@
         <v-dialog v-model="dialog" width="auto">
             <v-container class="dialog-container">
                 <v-row v-for="feedback in feedbackList" :key="feedback.id">
-                    <v-card style="margin-top:5px;margin-bottom: 5px;">
+                    <v-card style="margin-top:5px;margin-bottom: 5px;width:100%;">
                         <v-card-title>{{ feedback.title }}</v-card-title>
-                        <v-card-subtitle>{{ feedback.feedbackTime }}</v-card-subtitle>
-                        
+                        <v-card-subtitle>{{ formatDateTime(feedback.feedbackTime) }}</v-card-subtitle>
+
                         <v-row align="center" class="mx-0">
-                            <v-rating :model-value="feedback.rating" color="amber" density="compact" half-increments readonly
-                                size="x-small" style="margin-left:15px;"></v-rating>
+                            <v-rating :model-value="feedback.rating" color="amber" density="compact" half-increments
+                                readonly size="x-small" style="margin-left:15px;"></v-rating>
                             <div class="text-grey ms-4 text-caption" style="position:relative;margin-top:8px;">
-                                {{feedback.rating}}
+                                {{ feedback.rating }}
                             </div>
                         </v-row>
 
                         <v-card-text>
-                            {{ feedback.content }}
+                            {{ feedback.feedbackContent }}
                         </v-card-text>
 
                     </v-card>
                 </v-row>
+                <div v-if="feedbackLength" class="text-center">暂时没有反馈哦～</div>
                 <v-card-actions>
                     <v-btn color="deep-purple-lighten-2" block @click="dialog = false">关闭对话框</v-btn>
                 </v-card-actions>
@@ -34,22 +35,60 @@
 </template>
   
 <script>
+import { axiosForActivity } from "@/main";
+import { formatDateTime } from "@/tools/Format.js";
+
 export default {
     props: {
         button: {
             type: String,
             required: true,
         },
-        feedbackList: {
-            type: Array,
+        activity: {
+            type: Object,
             required: true,
         }
     },
     data() {
         return {
             dialog: false,
+            feedbackList: [],//获取每一个指定活动id的反馈
+            feedbackListLength:0,
         }
     },
+    computed:{
+        feedbackLength(){
+            return this.feedbackList.length==0;
+        }
+    },
+    mounted() {
+        this.getFeedbackList();
+        
+    },
+    methods: {
+        formatDateTime,
+        reserve() {
+            this.loading = true
+
+            setTimeout(() => (this.loading = false), 2000)
+        },
+        getFeedbackList() {
+            console.log(this.activity);
+            console.log(this.activity.id);
+            const activityId = this.activity.id;
+            axiosForActivity.get(`/api/activity/feedbacks/activity/${activityId}`) // 替换为您的API端点
+                .then(response => {
+                    console.log(response);
+
+                    this.feedbackList=response.data;
+                })
+                .catch(error => {
+                    console.error('Error fetching activities:', error);
+                    // 可以添加错误处理逻辑
+                });
+        }
+    }
+
 }
 </script>
   
