bbff6458704f9af5ad63eea942be8025f4f6b697
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2024-01-12 11:45:15 +0800

    个人中心框架初步

--- FootprintWalker/src/components/ShowReplyButton.vue
+++ FootprintWalker/src/components/ShowReplyButton.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center">
-        <v-btn color="deep-purple-lighten-2" @click="dialog = true">
+        <v-btn color="deep-purple-lighten-2" @click="getReply">
             查看反馈进度
         </v-btn>
 
@@ -36,6 +36,7 @@
 </template>
 <script>
 import { formatDateTime } from "@/tools/Format.js";
+import { axiosForActivity } from "@/main";
 
 export default {
     props: {
@@ -47,13 +48,13 @@ export default {
     data() {
         return {
             dialog: false,
-            reply:{
-                content:"",  //回复内容
-                replyTime:"",//回复时间
+            reply: {
+                content: "",  //回复内容
+                replyTime: "",//回复时间
             },
         }
     },
-    methods:{
+    methods: {
         formatDateTime,
         reserve() {
             this.loading = true
@@ -62,8 +63,21 @@ export default {
         },
 
         //需要根据反馈id获取对应的回复
-        getReply(){
-
+        getReply() {
+            this.dialog = true;
+            axiosForActivity.get(`/api/activity/replies/feedback/${this.myFeedback.id}`)
+                .then(response => {
+                    console.log(response);
+                    this.reply = response.data.map(myfeedback => ({
+                        content: myfeedback.replyContent,
+                        replyTime:myfeedback.replyTime
+                    }));
+                    console.log(this.reply)
+                })
+                .catch(error => {
+                    console.error('Error fetching replies:', error);
+                    // 可以添加错误处理逻辑
+                });
         }
     }
 }
--- FootprintWalker/src/views/PersonalCenter.vue
+++ FootprintWalker/src/views/PersonalCenter.vue
@@ -3,30 +3,44 @@
     <div style="position: relative;">
         <!-- 顶部的导航栏 -->
         <DefaultBar style="position: fixed; top: 0; left: 0; z-index: 5;" />
-        <!-- 图片元素 -->
-        <!-- <img src="../assets/个人中心背景.png" style="position: absolute; top: 0; left: 0; width: 100%; z-index: 1;" alt=""> -->
-        <!-- 显示个人信息 -->
         <v-card style="position:relative;height:100%;">
             <v-layout>
                 <!-- 抽屉式导航栏 -->
                 <v-navigation-drawer expand-on-hover rail>
+                    <!-- 显示头像和邮箱 -->
                     <v-list>
-                        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Sandra Adams"
-                            subtitle="sandra_a88@gmailcom"></v-list-item>
+                        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+                            :title="personalInfo.name" :subtitle="personalInfo.email"></v-list-item>
                     </v-list>
 
                     <v-divider></v-divider>
 
                     <v-list density="compact" nav>
-                        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-                        <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me"
-                            value="shared"></v-list-item>
-                        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+                        <v-list-item  @click="setCurrentOption('userInfo')" prepend-icon="mdi-account" title="个人信息" value="userInfo"></v-list-item>
+                        <v-list-item @click="setCurrentOption('activity')" prepend-icon="mdi-balloon" title="已报名的活动" value="activity"></v-list-item>
+                        <v-list-item @click="setCurrentOption('setting')" prepend-icon="mdi-cog-outline" title="修改个人信息" value="setting"></v-list-item>
                     </v-list>
                 </v-navigation-drawer>
 
                 <v-main style="positon:absolute;height: 800px;">
-                
+                    <!-- 根据当前选项显示不同的内容 -->
+                    <div v-if="currentOption === 'userInfo'">
+                        <!-- 个人信息页面的内容 -->
+                        个人信息
+                        <!-- ... -->
+                    </div>
+
+                    <div v-if="currentOption === 'activity'">
+                        <!-- 已报名的活动页面的内容 -->
+                        已报名活动
+                        <!-- ... -->
+                    </div>
+
+                    <div v-if="currentOption === 'setting'">
+                        <!-- 修改个人信息页面的内容 -->
+                        修改个人信息
+                        <!-- ... -->
+                    </div>
                 </v-main>
             </v-layout>
         </v-card>
@@ -53,20 +67,23 @@ export default {
         userId: 1,
         password: null,
         loading: false,
-        personalInfo:{
-            name:"",
-            gender:"",
-            email:"",
-            phoneNumber:"",
-            campus:"",
-            institute:"",
-            major:"",
+        currentOption: 'userInfo', // 默认选项为个人信息
+        personalInfo: {
+            name: "deidei",           //用户姓名
+            gender: "女",         //用户性别
+            email: "2051498@tongji.edu.cn",          //用户邮箱
+            phoneNumber: "15961672163",    //用户电话号码
+            campus: "嘉定",         //用户校区
+            institute: "软件学院",      //用户学院
+            major: "软件工程",          //用户专业
         },
 
 
     }),
     methods: {
-
+        setCurrentOption(option) {
+            this.currentOption = option;
+        },
     },
 }
 
